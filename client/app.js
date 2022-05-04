90964eec12dd88c9bd7a1b5d5df4e6f483d74c26
Bibin Babu <bibin6724@gmail.com>
2022-05-04 21:21:18 +0530

    to create java script file we need to add the url from flask server.so lot it stay here and we can continue working on the back end

---
+++ client/app.js
@@ -0,0 +1,7 @@
+//java script
+function onClickedCreateQRC() {
+    console.log("Generate QR button clicked");
+    var qrd = document.getElementById("qrd");
+    alert("the data for QR is :\n" +qrd.value)
+    
+}
